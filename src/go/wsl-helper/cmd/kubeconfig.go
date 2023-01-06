45ff65548a5734ab9d609d9c5c2baf655b74e6a3
Eric Promislow <epromislow@suse.com>
2023-01-06 13:34:30 -0800

    WIP: allow parent symlink
    Signed-off-by: Eric Promislow <epromislow@suse.com>

--- src/go/wsl-helper/cmd/kubeconfig.go
+++ src/go/wsl-helper/cmd/kubeconfig.go
@@ -61,50 +61,24 @@ var kubeconfigCmd = &cobra.Command{
 		if show {
 			// The output is "true", "false", or an error message for UI.
 			// We will only return nil in this path.
-			target, err := os.Readlink(linkPath)
-			if err != nil {
-				if errors.Is(err, os.ErrNotExist) {
-					fmt.Println("false")
-				} else if errors.Is(err, syscall.EINVAL) {
-					fmt.Printf("File %s exists and is not a symlink\n", linkPath)
-				} else {
-					fmt.Printf("%s\n", err)
-				}
-			} else if target == configPath {
-				fmt.Println("true")
-			} else {
-				// For a symlink pointing elsewhere, we assume we can overwrite.
-				fmt.Println("false")
-			}
+			fmt.Println(getLinkStatus(configDir, linkPath, configPath))
 			return nil
 		}
 		if enable {
-			err = os.Mkdir(configDir, 0o750)
-			if err != nil && !errors.Is(err, os.ErrExist) {
-				// The error already contains the full path, we can't do better.
-				return err
-			}
-			err = os.Symlink(configPath, linkPath)
-			if err != nil {
-				if errors.Is(err, os.ErrExist) {
-					// If it already exists, do nothing; even if it's not a symlink.
-					return nil
-				}
-				return err
-			}
+			return createLinkToLocalConfig(configDir, linkPath, configPath)
 		} else {
-			// No need to create if we want to remove it
-			target, err := os.Readlink(linkPath)
-			if err != nil {
-				if errors.Is(err, os.ErrNotExist) {
+			return deleteLinkToLocalConfig(configDir, linkPath, configPath)
+			if errors.Is(err, syscall.EINVAL) {
+				// See if the source's parent dir is a link to the target's parent dir
+				target, err := os.Readlink(configDir)
+				if err != nil {
 					return nil
 				}
-				return err
-			}
-			if target == configPath {
-				err = os.Remove(linkPath)
-				if err != nil && !errors.Is(err, os.ErrNotExist) {
-					return err
+				if target == path.Dir(configPath) {
+					err = os.Remove(linkPath)
+					if err != nil && !errors.Is(err, os.ErrNotExist) {
+						return err
+					}
 				}
 			}
 		}
@@ -119,4 +93,72 @@ func init() {
 	kubeconfigViper.AutomaticEnv()
 	kubeconfigViper.BindPFlags(kubeconfigCmd.PersistentFlags())
 	rootCmd.AddCommand(kubeconfigCmd)
+}
+
+// configDir = path.Parent(linkPath)
+func getLinkStatus(configDir string, linkPath string, configPath string) string {
+	target, err := os.Readlink(linkPath)
+	if err == nil {
+		// For a symlink pointing elsewhere, we assume we can overwrite.
+		return fmt.Sprintf("%v", target == configPath)
+	}
+	if errors.Is(err, os.ErrNotExist) {
+		return "false"
+	}
+	if errors.Is(err, syscall.EINVAL) {
+		// See if the source's parent dir is a link to the target's parent dir
+		target, err := os.Readlink(configDir)
+		if err != nil {
+			return fmt.Sprintf("File %s exists and is not a symlink", linkPath)
+		}
+		// If ~/.kube => the local WSL /DRIVE/mnt/Users/USER/.kube, it's good
+		return fmt.Sprintf("%v", target == path.Dir(configPath))
+	}
+	return err.Error()
+}
+
+func createLinkToLocalConfig(configDir string, linkPath string, configPath string) error {
+	err = os.Mkdir(configDir, 0o750)
+	if err != nil && !errors.Is(err, os.ErrExist) {
+		// The error already contains the full path, we can't do better.
+		return err
+	}
+	err = os.Symlink(configPath, linkPath)
+	if err != nil && errors.Is(err, os.ErrExist) {
+		// If it already exists, do nothing; even if it's not a symlink.
+		return nil
+	}
+	return err
+}
+
+func deleteLinkToLocalConfig(configDir string, linkPath string, configPath string) error {
+	// No need to create if we want to remove it
+	target, err := os.Readlink(linkPath)
+	if err == nil {
+		if target == configPath {
+			err = os.Remove(linkPath)
+			if err != nil && !errors.Is(err, os.ErrNotExist) {
+				return err
+			}
+		}
+		return nil
+	}
+	if errors.Is(err, os.ErrNotExist) {
+		return nil
+	}
+	if !errors.Is(err, syscall.EINVAL) {
+		return err
+	}
+	// If ~/.kube is a link to the mounted /mnt/DRIVE/Users/USER/.kube, delete the link also.
+	target, err = os.Readlink(configDir)
+	if err != nil {
+		return nil
+	}
+	if target == path.Dir(configPath) {
+		err = os.Remove(linkPath)
+		if err != nil && !errors.Is(err, os.ErrNotExist) {
+			return err
+		}
+	}
+	return nil
 }
